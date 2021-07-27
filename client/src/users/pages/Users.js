1785b98dbbb4ba4adb67810784fd568624b6ea5d
Nicodemus Ngufuli <ngufuli@outlook.com>
2021-07-28 01:17:45 +0530

    .

---
+++ client/src/users/pages/Users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Users = () => {
+    return(
+        <div>
+            <p>Welcome to places.io, share your places.</p>
+            <p>The home page of sharing all king of content</p>
+        </div>
+        
+    )
+}
+
+export default Users;
